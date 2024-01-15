86acf4bca10e5be00f28c9317845cd81b7acd682
Andres Ramirez <ramirezsilva,felipe.silva@gmail.com>
2024-01-15 10:37:46 -0600

    UI
    Created all visual elements

--- src/app/modules/content/content/content.component.ts
+++ src/app/modules/content/content/content.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
-
+  cardsData = Array.from({ length: 12 }, (_, index) => ({
+    id: `#001`,
+    imageUrl: `../../../../assets/img/1.png`,
+    pokemonId: `#001`,
+    pokemonName: `Bulbasor`,
+    types: ['Grass', 'Poison'],
+    stats: ['7M', '69KG']
+  }));
 }
--- src/app/modules/fixed/filter/filter.component.ts
+++ src/app/modules/fixed/filter/filter.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./filter.component.css']
 })
 export class FilterComponent {
-
+  filterTypes = ['normal', 'fire', 'water', 
+                'grass', 'electric', 'ice', 
+                'fighting', 'poison', 'ground', 
+                'flying', 'psychic', 'bug', 'rock', 
+                'ghost', 'dark', 'dragon', 'steel', 
+                'fairy'];
 }
