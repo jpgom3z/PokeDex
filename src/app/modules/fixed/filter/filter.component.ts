ab06365f2ce6b624b5d0ba3da16ef89ed45e4d6b
jpgom3z <jpgom29@gmail.com>
2024-01-15 22:48:34 -0600

    Filter service con errores

--- src/app/modules/fixed/filter/filter.component.ts
+++ src/app/modules/fixed/filter/filter.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TypeData, TypeResults } from '@models/type';
+import { FilterService } from '@services/filter.service';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.css']
 })
-export class FilterComponent {
-  filterTypes = ['normal', 'fire', 'water', 
-                'grass', 'electric', 'ice', 
-                'fighting', 'poison', 'ground', 
-                'flying', 'psychic', 'bug', 'rock', 
-                'ghost', 'dark', 'dragon', 'steel', 
-                'fairy'];
+export class FilterComponent implements OnInit {
+
+public data: TypeResults[];
+
+constructor(
+  private filterService: FilterService
+){
+  this.data = [];
+}
+
+public ngOnInit(): void {
+  this.filterService.list().subscribe((data) => {
+    this.data = data.results;
+    console.log(this.data);
+  });
+}
+  // filterTypes = ['normal', 'fire', 'water', 
+  //               'grass', 'electric', 'ice', 
+  //               'fighting', 'poison', 'ground', 
+  //               'flying', 'psychic', 'bug', 'rock', 
+  //               'ghost', 'dark', 'dragon', 'steel', 
+  //               'fairy'];
 }
