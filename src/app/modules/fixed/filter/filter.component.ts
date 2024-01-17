9bf4cdab6f053ed6ada8815f15ee59dc52814f1c
Andres Ramirez <ramirezsilva,felipe.silva@gmail.com>
2024-01-17 14:42:22 -0600

    Filtros estaticos
    Se modofica el Filter component para que sea un elemento estatico y no dependa del API query

--- src/app/modules/fixed/filter/filter.component.ts
+++ src/app/modules/fixed/filter/filter.component.ts
@@ -1,25 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { FilterService } from '@services/filter.service';
-// import { Subscription } from 'rxjs';
-import { TypeData, TypeResults } from '@models/type';
+// import { Component, OnInit } from '@angular/core';
+// import { FilterService } from '@services/filter.service';
+// // import { Subscription } from 'rxjs';
+// import { TypeData, TypeResults } from '@models/type';
+
+// @Component({
+//   selector: 'app-filter',
+//   templateUrl: './filter.component.html',
+//   styleUrls: ['./filter.component.css']
+// })
+
+// export class FilterComponent implements OnInit {
+
+//   public filters: TypeResults[];
+
+//   constructor( private filterService: FilterService) {this.filters = [];}
+
+//   // Solucion: El parametro data debia ser de tipo TypeData. 
+//   ngOnInit(){
+//     this.filterService.list().subscribe((data : TypeData) => {
+//       this.filters = data.results;
+//       console.log(this.filters)
+//     })
+//   }
+// }
+
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.css']
 })
-
-export class FilterComponent implements OnInit {
-
-  public filters: TypeResults[];
-
-  constructor( private filterService: FilterService) {this.filters = [];}
-
-  // Solucion: El parametro data debia ser de tipo TypeData. 
-  ngOnInit(){
-    this.filterService.list().subscribe((data : TypeData) => {
-      this.filters = data.results;
-      console.log(this.filters)
-    })
-  }
+export class FilterComponent {
+  filterTypes = ['normal', 'fire', 'water', 'grass', 
+                  'electric', 'ice', 'fighting', 'poison', 
+                  'ground', 'flying','psychic', 'bug',
+                  'rock', 'ghost', 'dark', 'dragon', 
+                  'steel', 'fairy', 'shadow'];
 }
